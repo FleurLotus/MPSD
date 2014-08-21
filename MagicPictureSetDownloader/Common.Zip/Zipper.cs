42f2d0e799fe6e570f3765f37d113346a6ecd5bb
fbossout@gmail.com <fbossout@gmail.com>
2014-08-21 10:38:47 +0200

    MPSD: Add autogeneration of database

---
+++ MagicPictureSetDownloader/Common.Zip/Zipper.cs
@@ -0,0 +1,47 @@
+﻿namespace Common.Zip
+{
+    using System.IO;
+
+    using ICSharpCode.SharpZipLib.Core;
+    using ICSharpCode.SharpZipLib.Zip;
+
+    public static class Zipper
+    {
+        public static Stream UnZipOneFile(byte[] stream)
+        {
+            ZipFile zipFile = null;
+            Stream outStream = null;
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                ms.Write(stream, 0, stream.Length);
+                zipFile = new ZipFile(ms);
+
+                foreach (ZipEntry entry in zipFile)
+                {
+                    if (!entry.IsFile)
+                        continue;
+
+                    byte[] buffer = new byte[4096];
+
+                    Stream zstream = zipFile.GetInputStream(entry);
+
+                    outStream = new MemoryStream(10 * 1024 * 1024);
+                    StreamUtils.Copy(zstream, outStream, buffer);
+                    outStream.Seek(0, SeekOrigin.Begin);
+                    break; // process the only one entry
+                }
+            }
+            finally
+            {
+                if (null != zipFile)
+                {
+                    zipFile.IsStreamOwner = true;
+                    zipFile.Close();
+                }
+            }
+
+            return outStream;
+        }
+    }
+}
---
+++ MagicPictureSetDownloader/MagicPictureSetDownloader.DbGenerator/GeneratorBase.cs
@@ -0,0 +1,46 @@
+﻿namespace MagicPictureSetDownloader.DbGenerator
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlServerCe;
+    using System.IO;
+
+    using Common.Zip;
+
+    internal class GeneratorBase
+    {
+        private readonly string _connectionString;
+
+        internal GeneratorBase(string connectionString)
+        {
+            _connectionString = connectionString;
+            SqlCeEngine engine = new SqlCeEngine(connectionString);
+            engine.CreateDatabase();
+
+        }
+        internal void Generate(byte[] stream)
+        {
+            StreamReader sr = new StreamReader(Zipper.UnZipOneFile(stream));
+            string sqlcommand = sr.ReadToEnd();
+            string[] commands = sqlcommand.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+
+            using (SqlCeConnection cnx = new SqlCeConnection(_connectionString))
+            {
+                cnx.Open();
+                using (SqlCeCommand cmd = cnx.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    foreach (string command in commands)
+                    {
+                        string trimcommand = command.TrimEnd(new[] { '\r', '\n' });
+                        if (!string.IsNullOrWhiteSpace(trimcommand))
+                        {
+                            cmd.CommandText = trimcommand;
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
