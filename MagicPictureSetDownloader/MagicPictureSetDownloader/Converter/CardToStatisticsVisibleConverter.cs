b6bfc75c5e97e6892946ab86e58267d77f454425
fbossout@gmail.com <fbossout@gmail.com>
2018-10-03 19:44:59 +0800

    MPSD (->10.7):
    	Correct some Editions code
    	Script correction of Guildgate version

--- MagicPictureSetDownloader/MagicPictureSetDownloader/Converter/CardToStatisticsVisibleConverter.cs
+++ MagicPictureSetDownloader/MagicPictureSetDownloader/Converter/CardToStatisticsVisibleConverter.cs
@@ -3,18 +3,14 @@ namespace MagicPictureSetDownloader.Converter
     using System;
     using System.Globalization;
     using System.Windows;
-
-    using Common.WPF.Converter;
-
+ 
     using MagicPictureSetDownloader.ViewModel.Main;
 
-    public class CardToStatisticsVisibleConverter : NoConvertBackMultiConverter
+    public class CardToStatisticsVisibleConverter : CardToStatisticsConverter
     {
-        private readonly CardToStatisticsConverter _innerConverter = new CardToStatisticsConverter();
-
-        public override object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+       public override object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            StatisticViewModel[] stats = _innerConverter.Convert(value, targetType, parameter, culture) as StatisticViewModel[];
+            StatisticViewModel[] stats = base.Convert(value, targetType, parameter, culture) as StatisticViewModel[];
 
             if (stats == null || stats.Length == 0)
             {
