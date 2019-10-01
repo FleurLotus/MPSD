fce741d1598b880561140c6997d27796250004a5
fbossout@gmail.com <fbossout@gmail.com>
2019-10-01 23:03:07 +0800

    Common (->6.3):
    Add AsyncCommand
    Add WebAccess async

---
+++ CommonLibraries/Common.Library/Threading/AsyncHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace Common.Library.Threading
+{
+    // Copyright (c) Microsoft Corporation, Inc. All rights reserved.
+    // Licensed under the MIT License, Version 2.0. See License.txt in the project root for license information.
+
+    using System;
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    //From https://github.com/aspnet/AspNetIdentity/blob/master/src/Microsoft.AspNet.Identity.Core/AsyncHelper.cs
+    public static class AsyncHelper
+    {
+        private static readonly TaskFactory myTaskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            var cultureUi = CultureInfo.CurrentUICulture;
+            var culture = CultureInfo.CurrentCulture;
+            return myTaskFactory.StartNew(() =>
+            {
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = cultureUi;
+                return func();
+            }).Unwrap().GetAwaiter().GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            var cultureUi = CultureInfo.CurrentUICulture;
+            var culture = CultureInfo.CurrentCulture;
+            myTaskFactory.StartNew(() =>
+            {
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = cultureUi;
+                return func();
+            }).Unwrap().GetAwaiter().GetResult();
+        }
+    }
+}
